8cf64d02359576f86972192e7f619161ef6a9c30
demedim <you@example.com>
2021-09-27 11:07:58 +0100

    implementation of LSM/M

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,33 @@
+#[allow(dead_code)]
+
+// maximum iteration count
+const MAX_ITER: i32 = 1000;
+// escaoe radius squared
+const RADIUS_SQ: f64 = 4.0;
+
+// scalar implementation of Level Set Method
+pub fn lsm(cr: f64, ci: f64) -> i32 {
+    // z (real and imaginary parts init)
+    let mut zr = 0.0;
+    let mut zi = 0.0;
+    // z^2 (real and imaginary parts init)
+    let mut zr2 = 0.0;
+    let mut zi2 = 0.0;
+    // iteration count
+    let mut iteration = 0;
+
+    while (iteration < MAX_ITER) && (zr2 + zi2 < RADIUS_SQ) {
+        // update z
+        zi = 2.0 * zr * zi + ci;
+        zr = zr2 - zi2 + cr;
+        // update z^2
+        zr2 = zr * zr;
+        zi2 = zi * zi;
+        // and update the iteration count
+        iteration = iteration + 1;
+    }
+    iteration
+}
+
 #[cfg(test)]
 mod tests {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
-fn main() -> Result<(), std::io::Error> {
+use oxidfract::lsm;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+// maximum iteration count
+const MAX_ITER: i32 = 1000;
+// width of the frame
+const WIDTH: usize = 1280;
+// height of the frame
+const HEIGHT: usize = 720;
+
+// mandelbrot range and domain
+const XMIN: f64 = -2.5;
+const XMAX: f64 = 1.0;
+const YMIN: f64 = -1.0;
+const YMAX: f64 = 1.0;
+
+fn render_mandelbrot() -> Vec<u8> {
+    let mut img_buffer: Vec<u8> = vec![0; WIDTH * HEIGHT * 3];
+
+    // main loop through all the pixels
+    for y in 0..HEIGHT as u32 {
+        for x in 0..WIDTH as u32 {
+            // mapping the pixel coordinates to the Mandelbrot domain
+            let (cr, ci) = (
+                (x as f64 / WIDTH as f64) * (XMAX - XMIN) + XMIN,
+                (y as f64 / HEIGHT as f64) * (YMAX - YMIN) + YMIN,
+            );
+            // calculate iterations
+            let iterations = lsm(cr, ci);
+
+            // set the pixels according to the iterations count
+            let pixel_r = (y as usize * WIDTH + x as usize) * 3;
+            let pixel_g = (y as usize * WIDTH + x as usize) * 3 + 1;
+            let pixel_b = (y as usize * WIDTH + x as usize) * 3 + 2;
+
+            if iterations == MAX_ITER {
+                img_buffer[pixel_r] = 0;
+                img_buffer[pixel_g] = 0;
+                img_buffer[pixel_b] = 0;
+            } else {
+                img_buffer[pixel_r] = 255;
+                img_buffer[pixel_g] = 255;
+                img_buffer[pixel_b] = 255;
+            }
+        }
+    }
+    img_buffer
+}
+
+fn write_file(data: Vec<u8>, filename: &str) -> std::io::Result<()> {
+    let path = Path::new(filename);
+    let mut file = File::create(&path)?;
+    let header = format!("P6 {} {} 255\n", WIDTH, HEIGHT);
+    file.write(header.as_bytes())?;
+    file.write(&data)?;
     Ok(())
+}
+fn main() -> Result<(), std::io::Error> {
+    let fractal = render_mandelbrot();
+    write_file(fractal, "out/fractal.ppm")
 }
