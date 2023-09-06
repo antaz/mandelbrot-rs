97cf5367b90b557586e3bb0860164575d5b5bccc
Antar Azri <azantar@proton.me>
2023-09-06 13:28:06 +0100

    Add command line interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,28 @@
-#[allow(dead_code)]
+use rayon::prelude::*;
+use std::mem::transmute;
 
-// maximum iteration count
 const MAX_ITER: i32 = 1000;
-// escaoe radius squared
 const RADIUS_SQ: f64 = 4.0;
+const WIDTH: usize = 1280;
+const HEIGHT: usize = 720;
 
-// scalar implementation of Level Set Method
+const XMIN: f64 = -2.5;
+const XMAX: f64 = 1.0;
+const YMIN: f64 = -1.0;
+const YMAX: f64 = 1.0;
+
 pub fn lsm(cr: f64, ci: f64) -> i32 {
-    // z (real and imaginary parts init)
     let mut zr = 0.0;
     let mut zi = 0.0;
-    // z^2 (real and imaginary parts init)
     let mut zr2 = 0.0;
     let mut zi2 = 0.0;
-    // iteration count
     let mut iteration = 0;
 
     while (iteration < MAX_ITER) && (zr2 + zi2 < RADIUS_SQ) {
-        // update z
         zi = 2.0 * zr * zi + ci;
         zr = zr2 - zi2 + cr;
-        // update z^2
         zr2 = zr * zr;
         zi2 = zi * zi;
-        // and update the iteration count
         iteration = iteration + 1;
     }
     iteration
@@ -33,43 +32,115 @@ pub fn lsm(cr: f64, ci: f64) -> i32 {
 use std::arch::x86_64::*;
 #[target_feature(enable = "avx2")]
 pub unsafe fn lsm_avx2(cr: __m256d, ci: __m256d) -> __m256d {
-    // z (real and imaginary parts init)
     let mut zr = _mm256_set1_pd(0.0);
     let mut zi = _mm256_set1_pd(0.0);
 
-    // z^2 (real and imaginary parts init)
     let mut zr2 = _mm256_set1_pd(0.0);
     let mut zi2 = _mm256_set1_pd(0.0);
 
-    // useful constants
     let one = _mm256_set1_pd(1.0);
     let two = _mm256_set1_pd(2.0);
     let four = _mm256_set1_pd(4.0);
 
-    // iteration counts
     let mut iterations = _mm256_set1_pd(0.0);
 
     for _ in 0..MAX_ITER {
-        // comparison mask of the magnitudes with the escape radius
         let mask = _mm256_cmp_pd::<_CMP_LT_OQ>(_mm256_add_pd(zr2, zi2), four);
 
-        // update the iteration counts
         iterations = _mm256_add_pd(_mm256_and_pd(mask, one), iterations);
 
-        // break if all values exceeded the threshold
         if _mm256_movemask_pd(mask) == 0 {
             break;
         }
 
-        // update z
         zi = _mm256_add_pd(_mm256_mul_pd(two, _mm256_mul_pd(zr, zi)), ci);
         zr = _mm256_add_pd(_mm256_sub_pd(zr2, zi2), cr);
 
-        // update z^2
         zr2 = _mm256_mul_pd(zr, zr);
         zi2 = _mm256_mul_pd(zi, zi);
     }
     iterations
+}
+
+pub fn render_mandelbrot(palette: &Vec<(u8, u8, u8)>) -> Vec<u8> {
+    let mut img_buffer: Vec<u8> = vec![0; WIDTH * HEIGHT * 3];
+
+    for y in 0..HEIGHT as u32 {
+        for x in 0..WIDTH as u32 {
+            let (cr, ci) = (
+                (x as f64 / WIDTH as f64) * (XMAX - XMIN) + XMIN,
+                (y as f64 / HEIGHT as f64) * (YMAX - YMIN) + YMIN,
+            );
+            let iterations = lsm(cr, ci);
+
+            let pixel_r = (y as usize * WIDTH + x as usize) * 3;
+            let pixel_g = (y as usize * WIDTH + x as usize) * 3 + 1;
+            let pixel_b = (y as usize * WIDTH + x as usize) * 3 + 2;
+
+            if iterations == MAX_ITER {
+                img_buffer[pixel_r] = 0;
+                img_buffer[pixel_g] = 0;
+                img_buffer[pixel_b] = 0;
+            } else {
+                img_buffer[pixel_r] =
+                    palette[iterations as usize % palette.len()].0;
+                img_buffer[pixel_g] =
+                    palette[iterations as usize % palette.len()].1;
+                img_buffer[pixel_b] =
+                    palette[iterations as usize % palette.len()].2;
+            }
+        }
+    }
+    img_buffer
+}
+
+pub fn render_parallel_mandelbrot(palette: &Vec<(u8, u8, u8)>) -> Vec<u8> {
+    let mut img_buffer: Vec<u8> = vec![0; WIDTH * HEIGHT * 3];
+
+    img_buffer
+        .par_chunks_exact_mut(WIDTH * 3)
+        .enumerate()
+        .for_each(|(y, rows)| {
+            rows.chunks_exact_mut(12)
+                .enumerate()
+                .for_each(|(c, chunk)| {
+                    let c = (c as f64) * 4.0;
+                    let y = y as f64;
+
+                    let cr = &mut [c, c + 1.0, c + 2.0, c + 3.0];
+                    let ci = &mut [y; 4];
+
+                    for (cr, ci) in cr.iter_mut().zip(ci.iter_mut()) {
+                        *cr = (*cr / WIDTH as f64) * (XMAX - XMIN) + XMIN;
+                        *ci = (*ci / HEIGHT as f64) * (YMAX - YMIN) + YMIN;
+                    }
+
+                    let iterations: [f64; 4] = unsafe {
+                        transmute(lsm_avx2(transmute(*cr), transmute(*ci)))
+                    };
+                    chunk.chunks_exact_mut(3).enumerate().for_each(
+                        |(t, triplet)| {
+                            if iterations[t] == MAX_ITER as f64 {
+                                triplet[0] = 0;
+                                triplet[1] = 0;
+                                triplet[2] = 0;
+                            } else {
+                                triplet[0] = palette
+                                    [iterations[t] as usize % palette.len()]
+                                .0;
+                                triplet[1] = palette
+                                    [iterations[t] as usize % palette.len()]
+                                .1;
+                                triplet[2] = palette
+                                    [iterations[t] as usize % palette.len()]
+                                .2;
+                            }
+                        },
+                    )
+                });
+        });
+
+    img_buffer
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,29 @@
-use oxidfract::{lsm, lsm_avx2};
-use rayon::prelude::*;
+use mandelbrot_rs::render_mandelbrot;
 use std::fs::File;
 use std::io::prelude::*;
-use std::mem::transmute;
 use std::path::Path;
 
-// maximum iteration count
-const MAX_ITER: i32 = 1000;
-// width of the frame
-const WIDTH: usize = 1280;
-// height of the frame
-const HEIGHT: usize = 720;
-
-// mandelbrot range and domain
-const XMIN: f64 = -2.5;
-const XMAX: f64 = 1.0;
-const YMIN: f64 = -1.0;
-const YMAX: f64 = 1.0;
-
-fn render_mandelbrot(palette: Vec<(u8, u8, u8)>) -> Vec<u8> {
-    let mut img_buffer: Vec<u8> = vec![0; WIDTH * HEIGHT * 3];
-
-    // main loop through all the pixels
-    for y in 0..HEIGHT as u32 {
-        for x in 0..WIDTH as u32 {
-            // mapping the pixel coordinates to the Mandelbrot domain
-            let (cr, ci) = (
-                (x as f64 / WIDTH as f64) * (XMAX - XMIN) + XMIN,
-                (y as f64 / HEIGHT as f64) * (YMAX - YMIN) + YMIN,
-            );
-            // calculate iterations
-            let iterations = lsm(cr, ci);
-
-            // set the pixels according to the iterations count
-            let pixel_r = (y as usize * WIDTH + x as usize) * 3;
-            let pixel_g = (y as usize * WIDTH + x as usize) * 3 + 1;
-            let pixel_b = (y as usize * WIDTH + x as usize) * 3 + 2;
-
-            if iterations == MAX_ITER {
-                img_buffer[pixel_r] = 0;
-                img_buffer[pixel_g] = 0;
-                img_buffer[pixel_b] = 0;
-            } else {
-                img_buffer[pixel_r] = palette[iterations as usize % palette.len()].0;
-                img_buffer[pixel_g] = palette[iterations as usize % palette.len()].1;
-                img_buffer[pixel_b] = palette[iterations as usize % palette.len()].2;
-            }
-        }
-    }
-    img_buffer
-}
-
-fn render_parallel_mandelbrot(palette: Vec<(u8, u8, u8)>) -> Vec<u8> {
-    let mut img_buffer: Vec<u8> = vec![0; WIDTH * HEIGHT * 3];
-
-    img_buffer
-        .par_chunks_exact_mut(WIDTH * 3)
-        .enumerate()
-        .for_each(|(y, rows)| {
-            rows.chunks_exact_mut(12)
-                .enumerate()
-                .for_each(|(c, chunk)| {
-                    let c = (c as f64) * 4.0;
-                    let y = y as f64;
-
-                    let cr = &mut [c, c + 1.0, c + 2.0, c + 3.0];
-                    let ci = &mut [y; 4];
-
-                    for (cr, ci) in cr.iter_mut().zip(ci.iter_mut()) {
-                        *cr = (*cr / WIDTH as f64) * (XMAX - XMIN) + XMIN;
-                        *ci = (*ci / HEIGHT as f64) * (YMAX - YMIN) + YMIN;
-                    }
-
-                    let iterations: [f64; 4] =
-                        unsafe { transmute(lsm_avx2(transmute(*cr), transmute(*ci))) };
-                    chunk
-                        .chunks_exact_mut(3)
-                        .enumerate()
-                        .for_each(|(t, triplet)| {
-                            if iterations[t] == MAX_ITER as f64 {
-                                triplet[0] = 0;
-                                triplet[1] = 0;
-                                triplet[2] = 0;
-                            } else {
-                                triplet[0] = palette[iterations[t] as usize % palette.len()].0;
-                                triplet[1] = palette[iterations[t] as usize % palette.len()].1;
-                                triplet[2] = palette[iterations[t] as usize % palette.len()].2;
-                            }
-                        })
-                });
-        });
-
-    img_buffer
-}
-
-fn write_file(data: Vec<u8>, filename: &str) -> std::io::Result<()> {
-    let path = Path::new(filename);
+fn write_file(
+    data: Vec<u8>,
+    path: &str,
+    width: usize,
+    height: usize,
+) -> std::io::Result<()> {
+    let path = Path::new(path);
     let mut file = File::create(&path)?;
-    let header = format!("P6 {} {} 255\n", WIDTH, HEIGHT);
+    let header = format!("P6 {} {} 255\n", width, height);
     file.write(header.as_bytes())?;
     file.write(&data)?;
     Ok(())
 }
-fn main() -> Result<(), std::io::Error> {
+fn main() -> () {
+    let dim = std::env::args().nth(1).expect("Expected dimensions");
+    let mut dim = dim.split("x");
+
+    let width: usize = dim.next().unwrap().parse().unwrap();
+    let height: usize = dim.next().unwrap().parse().unwrap();
+    let path = std::env::args().nth(2).expect("Expected path");
+
     let palette = vec![
         (38, 70, 83),
         (42, 157, 143),
@@ -110,6 +31,7 @@ fn main() -> Result<(), std::io::Error> {
         (244, 162, 97),
         (231, 111, 81),
     ];
-    let fractal = render_parallel_mandelbrot(palette);
-    write_file(fractal, "out/fractal.ppm")
+
+    let data = render_mandelbrot(&palette);
+    write_file(data, &path, width, height).unwrap();
 }
