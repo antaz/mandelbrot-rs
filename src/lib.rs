831b247daaa5a8751b378efa9e42013316dac7ac
Antar Azri <azantar@proton.me>
2023-09-07 15:36:51 +0100

    Add rayon parallelization

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use rayon::prelude::*;
+
 const MAX_ITER: i32 = 1000;
 const RADIUS_SQ: f64 = 4.0;
 
@@ -23,21 +25,24 @@ pub fn lsm(cr: f64, ci: f64) -> i32 {
     iteration
 }
 pub fn render_mandelbrot(buffer: &mut [u32], width: usize, height: usize) {
-    buffer.chunks_mut(width).enumerate().for_each(|(y, rows)| {
-        rows.iter_mut().enumerate().for_each(|(x, pixel)| {
-            let (cr, ci) = (
-                (x as f64 / width as f64) * (XMAX - XMIN) + XMIN,
-                (y as f64 / height as f64) * (YMAX - YMIN) + YMIN,
-            );
-            let iterations = lsm(cr, ci);
+    buffer
+        .par_chunks_mut(width)
+        .enumerate()
+        .for_each(|(y, rows)| {
+            rows.iter_mut().enumerate().for_each(|(x, pixel)| {
+                let (cr, ci) = (
+                    (x as f64 / width as f64) * (XMAX - XMIN) + XMIN,
+                    (y as f64 / height as f64) * (YMAX - YMIN) + YMIN,
+                );
+                let iterations = lsm(cr, ci);
 
-            if iterations == MAX_ITER {
-                *pixel = 0 | (0 << 8) | (0 << 16);
-            } else {
-                *pixel = 255 | (255 << 8) | (255 << 16);
-            }
-        })
-    });
+                if iterations == MAX_ITER {
+                    *pixel = 0 | (0 << 8) | (0 << 16);
+                } else {
+                    *pixel = 255 | (255 << 8) | (255 << 16);
+                }
+            })
+        });
 }
 
 #[cfg(test)]
